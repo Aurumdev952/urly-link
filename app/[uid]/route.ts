1cc43c343ccc879ba29d661021920f83269f2f4e
aurumdev952 <117060315+Aurumdev952@users.noreply.github.com>
2023-08-14 01:46:49 +0200

    added edge runtime

--- app/[uid]/route.ts
+++ app/[uid]/route.ts
@@ -1,6 +1,8 @@
 import { getUrl } from "@/server/controllers";
 import { redirect } from "next/navigation";
 
+export const runtime = 'edge'
+
 export async function GET(
   request: Request,
   { params }: { params: { uid: string } }
